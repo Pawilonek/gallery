93ef0440f6335733c0e497b7284dd03eaba66229
pakaminski <pakaminski@PGS198.pgs-soft.com>
2015-03-05 10:48:58 +0100

    unfinisched login popup

--- js/app.js
+++ js/app.js
@@ -47,3 +47,45 @@ angular.module('galleryApp').controller('AlertCtrl', ['$scope', '$rootScope', fu
         $scope.alerts.push(param);
     });
 }]);
+
+angular.module('galleryApp').controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+  $scope.open = function () {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'loginForm.html',
+      controller: 'authenticationsCtrl',
+      resolve: {
+        items: function () {
+          //return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+/*
+angular.module('galleryApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+    $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+});
+*/
